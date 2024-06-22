048c435333a8e6a3c8ff908000e48381cd3419cb
Chandra170695 <67903979+Chandra170695@users.noreply.github.com>
2024-06-22 11:04:43 +0530

    add

--- src/services/dataServices.js
+++ src/services/dataServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = 'http://localhost:5000/api/dummy-stock-data';
-const API_URL = `${process.env.REACT_APP_API_URL}/api/dummy-stock-data`;
+const API_URL = 'https://dummy-stock-api.vercel.app/api/dummy-stock-data';
+//const API_URL = `${process.env.REACT_APP_API_URL}/api/dummy-stock-data`;
 const fetchData = async () => {
     try {
         const response = await axios.get(API_URL); 
