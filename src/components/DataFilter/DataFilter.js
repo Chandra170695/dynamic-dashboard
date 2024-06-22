efdae93ab59a6cf89514e403e538684e04ffed2a
Chandra170695 <67903979+Chandra170695@users.noreply.github.com>
2024-06-22 11:26:01 +0530

    added changes

--- src/components/DataFilter/DataFilter.js
+++ src/components/DataFilter/DataFilter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DataFilter = ({ options, onSelect }) => (
-  <select onChange={(e) => onSelect(e.target.value)}>
+  <select className="select-dropdown"  onChange={(e) => onSelect(e.target.value)}>
     {options.map((option, index) => (
       <option key={index} value={option}>
         {option}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,12 @@ import styled from 'styled-components';
 const HeaderContainer = styled.header`
   text-align: center;
   padding: 20px;
-  background-color: #f5f5f5;
 `;
 
 const Header = () => (
   <HeaderContainer>
-    <h1>Dynamic Dashboard</h1>
-    <p>A React dashboard displaying dynamic data</p>
+    <h1 className="header">Stock Exchange Data</h1>
+    <p  className="para">Explore simulated stock exchange data featuring daily open, high, low, and close values.</p>
   </HeaderContainer>
 );
 
